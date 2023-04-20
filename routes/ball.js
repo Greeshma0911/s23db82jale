0ecec6a0bb8953ffc2d7df49a4561985f22f529c
Greeshma0911 <greeshmajale6520@gmail.com>
2023-04-19 19:25:15 -0500

    commit

--- routes/ball.js
+++ routes/ball.js
@@ -9,4 +9,9 @@ router.get('/', ball_controlers.ball_view_all_Page );
 router.get('/detail', ball_controlers.ball_view_one_Page);
 /* GET create ball page */
 router.get('/create', ball_controlers.ball_create_Page);
+/* GET create update page */
+router.get('/update', ball_controlers.ball_update_Page);
+/* GET delete ball page */
+router.get('/delete', ball_controlers.ball_delete_Page);
+
 module.exports = router;
